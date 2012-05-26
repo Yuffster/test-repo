bfc6951683894436a90c9a3394f172d797a8ce5b
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-05-25 21:21:47 -0700

    ...

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "This is not the greatest site in the world; this is just a fucking awesome continuous integration server..."
+  "This is not the greatest site in the world; this is just a fucking awesome continuous integration server..!"
 end
