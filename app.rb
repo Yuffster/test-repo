f5c5bc91514657bd0494325c3dd9bb7bc2bc6d2b
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-05-25 18:21:10 -0700

    Touch.

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "This is not the greatest site in the world; this is just an update."
+  "This is not the greatest site in the world; this is just an the production server"
 end
